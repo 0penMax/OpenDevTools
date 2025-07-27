5bf826d4bc12fcd5d99240f0834b7edab0126ff3
max <31370923+max75025@users.noreply.github.com>
2025-07-27 15:10:22 +0300

    add menu for select input method

---
+++ menus/inputMenu.go
@@ -0,0 +1,57 @@
+package menus
+
+import (
+	"github.com/pterm/pterm"
+)
+
+func showInputMenu(processF func(string) (string, error)) {
+	ClearScreen()
+
+	pterm.DefaultHeader.WithFullWidth().Println("Select input method")
+	pterm.Println()
+
+	navMap := make(map[string]func(processF func(string) (string, error)))
+	var menu []string
+
+	mi := "Multiline input"
+	navMap[mi] = showMultilineInput
+	menu = append(menu, mi)
+
+	clp := "Clipboard"
+	navMap[clp] = processFromClipboard
+	menu = append(menu, clp)
+
+	selectedOption, _ := pterm.DefaultInteractiveSelect.WithMaxHeight(10).WithOptions(menu).Show()
+
+	f := navMap[selectedOption]
+	f(processF)
+
+}
+
+func nilErrorWrapper(processF func(string) string) func(string) (string, error) {
+	return func(v string) (string, error) { return processF(v), nil }
+}
+
+func showMultilineInput(processF func(string) (string, error)) {
+	ClearScreen()
+	// Create a default interactive text input with multi-line enabled.
+	// This allows the user to input multiple lines of text.
+	textInput := pterm.DefaultInteractiveTextInput.WithMultiLine()
+
+	// Show the text input to the user and store the result.
+	// The second return value (an error) is ignored with '_'.
+	text, _ := textInput.Show()
+
+	// Print a blank line for better readability in the output.
+	pterm.Println()
+
+	result, err := processF(text)
+	if err != nil {
+		pterm.Warning.Println(err)
+		return
+	}
+
+	pterm.Info.Println("Result:")
+	pterm.Println(result)
+
+}
